a380ad20cfccda86d9e6b5aa7ad1ccb1b328ed67
LazyFaceDaniel <dguo83212@gmail.com>
2022-08-30 17:51:03 +0000

    added platform.js

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -43,13 +43,14 @@
     createPlatform(450, 535, 0.5, 0.1);
     createPlatform(600, 600, 0.5, 0.1);
     createPlatform(530, 460, 0.01);
-    createPlatform(600, 400, 0.01);
+    createPlatform(650, 360, 0.01);
+    createPlatform(560, 400, 0.01);
     createPlatform(10, 270, 0.7);
     createPlatform(800, 460, 0.3, 0.1);
     createPlatform(800, 350, 0.3, 0.1);
-    
+    createPlatform(440, 350, 0.01);
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
-  platform.init = init;
+  platform.init = init
 })(window);
